a5f017b3ffacbbb8e250b6c70053333af8721d16
testpujari <testpujari@gmail.com>
2025-12-12 22:13:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('Hii this is a node project ');
 });
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+ module.exports = app;
